1607c12f369a848ea48c513b6dee75ea98687c68
Pål Sollie <pal.sollie@telia.no>
2023-11-22 21:16:54 +0100

    Use envs as input to entrypoint

--- source/entrypoint.sh
+++ source/entrypoint.sh
@@ -2,8 +2,29 @@
 
 set -e
 
-labels=$(echo "$3" | yq eval-all '.[] | to_entries | map("\(.key)=\(.value)") | join(",")' -)
+parse_labels() {
+  local labels_string="$1"
+  local labels_json="{}"
 
-python -m pip install --upgrade pip
-pip install -r /requirements.txt
-python /script.py --name "$1" --namespace "$2" --labels "$labels" "${@:4}"
+  IFS=',' read -ra labels <<< "$labels_string"
+  for label in "${labels[@]}"; do
+    IFS=':' read -r key value <<< "$label"
+    labels_json="$labels_json,\"$key\":\"$value\""
+  done
+
+  echo "${labels_json:1}"  # Remove leading comma
+}
+
+output_option=""
+if [ -n "$INPUT_OUTPUT" ]; then
+  output_option="--output $INPUT_OUTPUT"
+fi
+
+parsed_labels=$(parse_labels "$INPUT_LABELS")
+
+python /script.py \
+  --name "$INPUT_NAME" \
+  --namespace "$INPUT_NAMESPACE" \
+  --labels "$parsed_labels" \
+  "$output_option" \
+  --file-paths "$INPUT_FILE_PATHS"
